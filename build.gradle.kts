f9064e7777eeac07e993f778cb66ba4d12ce0b37
Rico Apon <rico.apon@gmail.com>
2022-05-25 22:01:05 +0200

    Add Hamcrest for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.hamcrest:hamcrest:2.2")
 }
 
 tasks.getByName<Test>("test") {
